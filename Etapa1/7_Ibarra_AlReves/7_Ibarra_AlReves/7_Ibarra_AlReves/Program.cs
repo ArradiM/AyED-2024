0f1cbf456d42cb0b9217da40b24fd9b6c6f3b805
ArradiM <167466835+ArradiM@users.noreply.github.com>
2024-04-25 21:06:49 -0300

    Suebo carpeta Etapa1

---
+++ Etapa1/7_Ibarra_AlReves/7_Ibarra_AlReves/7_Ibarra_AlReves/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Ibarra_AlReves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escriba la primera letra");
+            string letra1;
+            letra1 = Console.ReadLine();
+            Console.WriteLine("La segunda");
+            string letra2;
+            letra2 = Console.ReadLine();
+            Console.WriteLine("La tercera");
+            string letra3;
+            letra3 = Console.ReadLine();
+
+            Console.WriteLine(letra3 + letra2 + letra1);
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
