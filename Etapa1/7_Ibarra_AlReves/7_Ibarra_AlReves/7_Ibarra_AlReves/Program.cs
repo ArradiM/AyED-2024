763e643956767f9857ce5f3ef0a17ab0646bb24f
ArradiM <167466835+ArradiM@users.noreply.github.com>
2024-05-09 17:37:42 -0300

    Subo carpeta  de Etapa1

---
+++ Etapa1/7_Ibarra_AlReves/7_Ibarra_AlReves/7_Ibarra_AlReves/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Ibarra_AlReves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string letra1;
+            Console.WriteLine("Escriba la primera letra");
+            letra1 = Console.ReadLine();
+            string letra2;
+            Console.WriteLine("La segunda");
+            letra2 = Console.ReadLine();
+            string letra3;
+            Console.WriteLine("La tercera");
+            letra3 = Console.ReadLine();
+
+            Console.WriteLine(letra3 + letra2 + letra1);
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ Etapa1/8_Ibarra_EsMayor/8_Ibarra_EsMayor/8_Ibarra_EsMayor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Ibarra_EsMayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int edad;
+            Console.WriteLine("¿cual es tu edad?");
+            edad = int.Parse(Console.ReadLine());
+
+            if(edad >= 18)
+            {
+                Console.WriteLine("eres mayor de edad");
+            } 
+            else
+            {
+                Console.WriteLine("eres menor de edad");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
