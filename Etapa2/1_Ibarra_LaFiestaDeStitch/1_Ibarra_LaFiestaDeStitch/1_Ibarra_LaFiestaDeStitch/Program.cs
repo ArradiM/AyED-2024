17ecddea54042d99daa3d8f644bbd414ae8b9e0b
ArradiM <167466835+ArradiM@users.noreply.github.com>
2024-05-28 16:49:31 -0300

    Subo carpeta de Etapa1

---
+++ Etapa2/1_Ibarra_LaFiestaDeStitch/1_Ibarra_LaFiestaDeStitch/1_Ibarra_LaFiestaDeStitch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Ibarra_LaFiestaDeStitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int invitados;
+            Console.WriteLine("Ingrese la cantidad de invitados ");
+            invitados = int.Parse(Console.ReadLine());
+            int[] vector = new int[invitados];
+
+            for (int i = 0; i < invitados; i++)
+            {
+                int comida;
+                Console.WriteLine("¿Que cantidad consume el invitado "+i + "?");
+                comida = int.Parse(Console.ReadLine());
+
+                if (comida >= 1 && comida<=100)
+                {
+                    
+                    vector[i] += comida;
+
+                  
+                }
+                else
+                {
+                    Console.WriteLine("error ");
+                    i--;
+                }
+            }
+            int acumulador = 0;
+
+            for (int i = 0; invitados > i; i++)
+            {
+                acumulador += vector[i];
+
+
+            }
+            Console.WriteLine("el porcentaje total es: " + acumulador / invitados);
+
+            Console.ReadKey();
+        }
+    }
+}
