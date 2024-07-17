7f86fef08c3bf4a5db1b0f5bee2b00c9929f1143
ArradiM <167466835+ArradiM@users.noreply.github.com>
2024-07-17 18:26:23 -0300

    Subo carpeta de Etapa2

---
+++ Etapa2/5_Ibarra_HotSale/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese la cantidad de productos vendidos durante el Hot Sale: ");
+        int productosvendidos=int.Parse(Console.ReadLine());
+
+        int[] productos= new int[productosvendidos];
+
+        for(int i =0;i<productosvendidos;i++){
+            Console.WriteLine("Ingrese el precio del producto N° "+(i + 1));
+            productos[i] = int.Parse(Console.ReadLine());
+        }
+
+
+        int preciomayor = productos[0];
+        int preciomenor = productos[0];
+
+
+        for (int i = 1; i < productosvendidos; i++)
+        {
+            if (productos[i] > preciomayor)
+            {
+                preciomayor = productos[i];
+            }
+            if (productos[i] < preciomenor)
+            {
+                preciomenor = productos[i];
+            }
+        }
+
+        
+        Console.WriteLine("El precio más alto registrado durante el evento es: "+ preciomayor);
+        Console.WriteLine("El precio más bajo registrado durante el evento es: "+preciomenor);
+        
+        Console.ReadKey();
+    }
+}
---
+++ Etapa2/6_Ibarra_ZEGARPG/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Ingrese la cantidad de elementos (objetos) que desea analizar: ");
+        int cantelementos = int.Parse(Console.ReadLine());
+
+        
+        Console.WriteLine("Ingrese el valor mínimo que deben tener los objetos para ser considerados en el filtro: ");
+        int valormin = int.Parse(Console.ReadLine());
+
+        
+        int[] elementos = new int[cantelementos];
+
+        
+        for (int i = 0; i < cantelementos; i++)
+        {
+            Console.WriteLine("Ingrese el valor del objeto N° " + (i + 1) + ": ");
+            elementos[i] = int.Parse(Console.ReadLine());
+        }
+
+        
+        Console.WriteLine("Las posiciones de los objetos cuyo valor es mayor al número especificado son:");
+        for (int i = 0; i < cantelementos; i++)
+        {
+            if (elementos[i] > valormin)
+            {
+                Console.WriteLine("Objeto en la posición " + (i + 1) + " con valor " + elementos[i]);
+            }
+        }
+
+        
+        Console.ReadKey();
+    }
+}
+
