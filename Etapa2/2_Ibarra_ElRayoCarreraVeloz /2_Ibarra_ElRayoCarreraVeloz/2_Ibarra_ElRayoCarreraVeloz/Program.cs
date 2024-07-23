726bfcd42c8b76b38af90186ccb8408ea44b05cb
ArradiM <167466835+ArradiM@users.noreply.github.com>
2024-07-23 13:18:39 -0300

    Subo carpeta de Etapa2

---
+++ Etapa2/2_Ibarra_ElRayoCarreraVeloz /2_Ibarra_ElRayoCarreraVeloz/2_Ibarra_ElRayoCarreraVeloz/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Ibarra_ElRayoCarreraVeloz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vueltas;
+            Console.WriteLine("Ingrese la cantidad de vueltas");
+            vueltas = int.Parse(Console.ReadLine());
+            int[] vector = new int[vueltas];
+
+            for (int i = 0; i < vueltas; i++)
+            {
+                
+                Console.WriteLine("¿CUANTO DURO LA CARRERA " +( i+1) + "?");
+                vector[i] += int.Parse(Console.ReadLine());
+                
+            }
+
+            
+            int acumulador = 0;
+
+            for (int i = 0; vueltas > i; i++)
+            {
+                acumulador += vector[i];
+
+            }
+
+            int mjvuelta = 0;
+            int tiempomin = vector[0];
+
+            for (int i = 0; i < vueltas; i++)
+            {
+                mjvuelta += vector[i];
+                if (vector[i] < tiempomin)
+                {
+                    tiempomin = vector[i];
+                    
+                }
+            }
+
+            Console.WriteLine("Tu puntaje total: " + acumulador);
+            Console.WriteLine("el Promedio " + acumulador / vueltas);
+            Console.WriteLine("la mejor vuelta : " +tiempomin);
+
+            Console.ReadKey();
+        }
+
+     }
+}
---
+++ Etapa2/7_Ibarra_ContratacionSoft/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int candidatos;
+        Console.WriteLine("Ingrese la cantidad de candidatos");
+        candidatos = int.Parse(Console.ReadLine());
+        int[] cantCandidatos = new int[candidatos];
+
+        for (int i = 0; i < candidatos; i++)
+        {
+            Console.WriteLine("Ingrese el puntaje del candidato N° " + (i + 1));
+            cantCandidatos[i] = int.Parse(Console.ReadLine());
+
+        }
+
+        Console.WriteLine("Ingrese un número entero ");
+        int valor = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Puntajes que son múltiplos de " + valor + ":");
+        for (int i = 0; i < cantCandidatos.Length; i++)
+        {
+            if (cantCandidatos[i] % valor == 0)
+            {
+                Console.WriteLine("Candidato N° " + (i + 1) + ": Puntaje " + cantCandidatos[i]);
+            }else
+            {
+                Console.WriteLine(cantCandidatos[i] +" No es multiplo de "+ valor);
+            }
+            
+        }
+
+
+
+        for (int i = 0; i < cantCandidatos.Length - 1; i++)
+        {
+            for (int j = 0; j < cantCandidatos.Length - i - 1; j++)
+            {
+                if (cantCandidatos[j] > cantCandidatos[j + 1])
+                {
+                    int aux = cantCandidatos[j];
+                    cantCandidatos[j] = cantCandidatos[j + 1];
+                    cantCandidatos[j + 1] = aux;
+                }
+            }
+        }
+
+
+        
+        Console.WriteLine("ORDEN DE MENOR A MAYOR: ");
+        for (int i = 0; i < candidatos; i++)
+        {
+            Console.WriteLine(cantCandidatos[i]);
+        }
+
+        Console.ReadKey();
+    }
+}
+
+
+
+
