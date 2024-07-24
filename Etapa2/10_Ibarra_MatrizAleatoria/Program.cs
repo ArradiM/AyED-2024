993c9bfd4301462365f48ffb01fc6c7059691441
ArradiM <167466835+ArradiM@users.noreply.github.com>
2024-07-24 15:35:04 -0300

    Subo carpeta de Etapa2

---
+++ Etapa2/10_Ibarra_MatrizAleatoria/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random aleatorio = new Random();
+        Console.WriteLine("Ingrese n para el número de filas ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese m para el número de columnas ");
+        int m = int.Parse(Console.ReadLine());
+
+
+        int[,] matriz = new int[n, m];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int opcion = aleatorio.Next(1, 100);
+                matriz[i,j]=opcion;
+            }
+        }
+
+
+        Console.WriteLine("\nMATRIZ:");
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(matriz[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ Etapa2/9_Ibarra_MiPrimerMatriz/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese n para el número de filas ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese m para el número de columnas ");
+        int m = int.Parse(Console.ReadLine());
+
+        int[,] matriz = new int[n, m];
+
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                matriz[i, j] = 0;
+            }
+        }
+
+        Console.WriteLine("\nMATRIZ:");
+
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(matriz[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
+
