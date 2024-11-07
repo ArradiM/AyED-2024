dfc964d6eef1eb5f2b0a5e9417bb4312c6db44cd
ArradiM <167466835+ArradiM@users.noreply.github.com>
2024-11-07 14:55:22 -0300

    Subo carpeta de Etapa4

---
+++ Etapa4/0_Ibarra_ConversorTemperaturasGUI/0_Ibarra_ConversorTemperaturasGUI/0_Ibarra_ConversorTemperaturasGUI/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace _0_Ibarra_ConversorTemperaturasGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.num = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.farenheit = new System.Windows.Forms.Label();
+            this.kelvin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Grados Celcius:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(149, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // num
+            // 
+            this.num.Location = new System.Drawing.Point(12, 68);
+            this.num.Name = "num";
+            this.num.Size = new System.Drawing.Size(100, 20);
+            this.num.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Farenheit:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(227, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kelvin:";
+            // 
+            // farenheit
+            // 
+            this.farenheit.AutoSize = true;
+            this.farenheit.Location = new System.Drawing.Point(314, 68);
+            this.farenheit.Name = "farenheit";
+            this.farenheit.Size = new System.Drawing.Size(26, 13);
+            this.farenheit.TabIndex = 5;
+            this.farenheit.Text = "0° F";
+            this.farenheit.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // kelvin
+            // 
+            this.kelvin.AutoSize = true;
+            this.kelvin.Location = new System.Drawing.Point(314, 99);
+            this.kelvin.Name = "kelvin";
+            this.kelvin.Size = new System.Drawing.Size(29, 13);
+            this.kelvin.TabIndex = 6;
+            this.kelvin.Text = "O° K";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 315);
+            this.Controls.Add(this.kelvin);
+            this.Controls.Add(this.farenheit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.num);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox num;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label farenheit;
+        private System.Windows.Forms.Label kelvin;
+    }
+}
+
